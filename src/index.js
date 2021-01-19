347d856cb3502def3ebaaf8834274a02cfbfacb4
jackie <99sharmatushar@gmail.com>
2021-01-19 23:13:03 +0530

    Make basic structure and registeration route

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import registerServiceWorker from './registerServiceWorker';
+
+import 'semantic-ui-css/semantic.min.css';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+const Root = () =>(
+    <Router>
+        <Switch>
+            <Route exact path="/" component={App}></Route>
+            <Route path="/register" component={Register}></Route>
+            <Route path="/login" component={Login}></Route>
+        </Switch>
+    </Router>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root'));
+registerServiceWorker();
