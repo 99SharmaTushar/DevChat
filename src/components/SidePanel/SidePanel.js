96ff6446a6b3b0c0268f3e1139f7916cc71ae073
jackie <99sharmatushar@gmail.com>
2021-02-14 23:04:16 +0530

    Do Message functionality

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Menu } from 'semantic-ui-react';
+
 import UserPanel from './UserPanel';
 import Channels from './Channels';
+import DirectMessages from './DirectMessages';
+import Starred from './Starred';
 
 class SidePanel extends React.Component {
     render(){
@@ -9,7 +12,9 @@ class SidePanel extends React.Component {
         return (
             <Menu size="large" inverted fixed="left" vertical style={{background: "#4c3c4c",fontSize: "1.2rem"}}>
                 <UserPanel currentUser={currentUser} />
-                <Channels currentUser={currentUser}/>
+                <Starred currentUser={currentUser} />
+                <Channels currentUser={currentUser} />
+                <DirectMessages currentUser={currentUser} />
             </Menu>
         )
     }
